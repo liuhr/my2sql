808dfa768da4a603dff936a59a0cfb8fbf9820a5
刘汉冉 <liuhanran5@163.com>
2022-09-05 09:52:40 +0800

    Update context.go
    修复-big-trx-row-limit、 -long-trx-seconds 取值范围

--- base/context.go
+++ base/context.go
@@ -52,8 +52,8 @@ var (
 
 	GOptsValueRange map[string][]int = map[string][]int{
 		"PrintInterval":  []int{1, 600, 30},
-		"BigTrxRowLimit": []int{1, 10, 30000, 500},
-		"LongTrxSeconds": []int{0, 1, 3600, 300},
+		"BigTrxRowLimit": []int{1, 30000, 10},
+		"LongTrxSeconds": []int{0, 3600, 1},
 		"InsertRows":     []int{1, 500, 30},
 		"Threads":        []int{1, 16, 2},
 	}
